bbfd86c7589ce09b013bd3d3336b5fa42e43ef93
Nazar <nazarnovosad610@gmail.com>
2021-04-14 20:29:56 +0300

    add dotenv

--- src/pages/Page1/Page1.tsx
+++ src/pages/Page1/Page1.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Page1 = () => {
   return (
     <div style={{ backgroundColor: 'red', width: '100vw', height: '100vh', fontSize: 40, color: '#fff' }}>
-      Page 1
+      Page 1<p>{process.env.REACT_APP_TEST_KEY}</p>
     </div>
   );
 };
