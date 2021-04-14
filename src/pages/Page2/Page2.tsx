513e58a9abef9ca0f723e225ce86577e8ec51dc8
Nazar <nazarnovosad610@gmail.com>
2021-04-14 20:20:30 +0300

    test2

--- src/pages/Page2/Page2.tsx
+++ src/pages/Page2/Page2.tsx
@@ -1,7 +1,11 @@
 import React from 'react';
 
 const Page2 = () => {
-  return <div style={{ backgroundColor: 'blue', width: '100vw', height: '100vh' }}>Page2</div>;
+  return (
+    <div style={{ backgroundColor: 'blue', width: '100vw', height: '100vh', fontSize: 40, color: '#fff' }}>
+      Page2
+    </div>
+  );
 };
 
 export default Page2;
